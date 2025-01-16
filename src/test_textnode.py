21cc8b5f999b753bd4670a37e141ee0e47032c21
AbdulKareem Mohammed <mabkareem@gmail.com>
2025-01-16 13:46:18 -0500

    Add intermediate TextNode class

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextType, TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node1 = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node1, node2)
+
+    def test_ineq(self):
+        node1 = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node1, node2)
+
+    def test_ineq_2(self):
+        node1 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node1, node2)
+
+    def test_eq_2(self):
+        node1 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        self.assertEqual(node1, node2)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
