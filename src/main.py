768dfa37b6ffcf687c492cf67d10c92dc24a1319
AbdulKareem Mohammed <mabkareem@gmail.com>
2025-01-19 14:12:43 -0500

    FEATURE: Add functionality to copy files from static to public

--- src/main.py
+++ src/main.py
@@ -2,11 +2,21 @@ import textnode
 import os
 import shutil
 
-def generate():
-    path = "../public"
-    if (os.path.exists(path)):
-        shutil.rmtree(path)
+def generate(source_path, destination_path):
+    if os.path.exists(destination_path):
+        shutil.rmtree(destination_path)
+    os.mkdir(destination_path)    
+    dir_contents = os.listdir(source_path)
+    for file in dir_contents:
+        file_path = os.path.join(source_path, file)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, destination_path)
+        else:
+            generate(file_path, os.path.join(destination_path, file))
+
+
 def main():
+    generate("./static", "./public")
     node_1 = textnode.TextNode("This is a text node", "bold")
     node_2 = textnode.TextNode("This is a text node", "italic", "https://www.boot.dev")
 
